00a20ccaf24033e4595f26d7a48e9a709aa0f256
Bernhard Maier <bernhard.maier@werbas-ag.com>
2020-09-01 15:41:14 +0200

    Added more component tests

--- src/app/book/book-list/book-list.component.spec.ts
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -1,18 +1,20 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { MatListModule } from '@angular/material/list';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { BookListComponent } from './book-list.component';
 import { BookDataService } from '../shared/book-data.service';
+import { of } from 'rxjs';
 
 describe('Component: BookList (<ws-book-list>)', () => {
   let fixture : ComponentFixture<BookListComponent>;
+  let bookDataService : BookDataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ BookListComponent ],
-      imports: [ MatListModule, RouterModule ],
+      imports: [ MatListModule, RouterTestingModule ],
       providers: [
         {
           provide: BookDataService,
@@ -29,6 +31,7 @@ describe('Component: BookList (<ws-book-list>)', () => {
     });
 
     fixture = TestBed.createComponent(BookListComponent);
+    bookDataService = TestBed.inject(BookDataService);
   });
 
   describe('When the component is displayed' , () => {
@@ -40,13 +43,22 @@ describe('Component: BookList (<ws-book-list>)', () => {
 
   describe('When the API delivers invalid data', () => {
     it('does not render any book', () => {
+      bookDataService.getBooks = () => of(null);
+      fixture.detectChanges();
 
+      const books : DebugElement[] = fixture.debugElement.queryAll(By.css('[data-testid=book-list_author]'));
+      expect(books.length).toBe(0);
     });
   });
 
   describe('When the API delivers valid data' , () => {
     it('displays the author name' , () => {
+      bookDataService.getBooks = () => of([{author:'Dummy1'}, {author:'Dummy2'}] as any);
+      fixture.detectChanges();
 
+      const books : DebugElement[] = fixture.debugElement.queryAll(By.css('[data-testid=book-list_author]'));
+      expect(books[0].nativeElement.innerHTML).toEqual('Dummy1');
+      expect(books[1].nativeElement.innerHTML).toEqual('Dummy2');
     });
   });
 });
