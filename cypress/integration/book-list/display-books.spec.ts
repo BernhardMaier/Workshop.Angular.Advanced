04488b4f08bfda39fdd1779ff06e72c6d81414e2
Bernhard Maier <bernhard.maier@werbas-ag.com>
2020-09-02 12:54:06 +0200

    Added fixture test

--- cypress/integration/book-list/display-books.spec.ts
+++ cypress/integration/book-list/display-books.spec.ts
@@ -4,33 +4,3 @@ describe('When the app has been started' , () => {
     cy.get('mat-toolbar').contains('Book Monkey');
   });
 });
-
-describe('Create book' , () => {
-  let isbn : string;
-
-  beforeEach(() => {
-    cy.visit('http://localhost:4200');
-    cy.get('[data-testid=book-list__book]').as('books');
-  });
-
-  afterEach(() => {
-    cy.request('DELETE', `http://localhost:4730/books/${isbn}`);
-  });
-
-  describe('When a new book is added' , () => {
-    it('displays the new book in the list' , () => {
-      isbn = `${Math.floor(1000000000000 + Math.random() * 900000)}`;
-      let booksCount = 0;
-      cy.get('@books')
-        .then(books => (booksCount = books.length))
-        .then(() => cy.get('[data-testid=nav_create]').click())
-        .then(() => cy.get('[formcontrolname=isbn]').type(isbn))
-        .then(() => cy.get('[formcontrolname=title]').type('Testing for Dummies'))
-        .then(() => cy.get('[formcontrolname=author]').type('Dummy Mc Dummy'))
-        .then(() => cy.get('[data-testid=book_create]').click())
-        .then(() => cy.get('[data-testid=nav_books]').click())
-        .then(() => cy.get('@books'))
-        .then(books => expect(books.length).to.eq(booksCount+1));
-    });
-  });
-});
