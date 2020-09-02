25422782e979444c05caa24e047fa326260aa9b3
Bernhard Maier <bernhard.maier@werbas-ag.com>
2020-09-02 11:17:10 +0200

    Added afterEach hook to delete newly created test books

--- cypress/integration/book-list/display-books.spec.ts
+++ cypress/integration/book-list/display-books.spec.ts
@@ -6,19 +6,25 @@ describe('When the app has been started' , () => {
 });
 
 describe('Create book' , () => {
+  let isbn : string;
+
   beforeEach(() => {
     cy.visit('http://localhost:4200');
     cy.get('[data-testid=book-list__book]').as('books');
   });
 
+  afterEach(() => {
+    cy.request('DELETE', `http://localhost:4730/books/${isbn}`);
+  });
+
   describe('When a new book is added' , () => {
     it('displays the new book in the list' , () => {
-      const isbn = Math.floor(1000000000000 + Math.random() * 900000);
+      isbn = `${Math.floor(1000000000000 + Math.random() * 900000)}`;
       let booksCount = 0;
       cy.get('@books')
         .then(books => (booksCount = books.length))
         .then(() => cy.get('[data-testid=nav_create]').click())
-        .then(() => cy.get('[formcontrolname=isbn]').type(`${isbn}`))
+        .then(() => cy.get('[formcontrolname=isbn]').type(isbn))
         .then(() => cy.get('[formcontrolname=title]').type('Testing for Dummies'))
         .then(() => cy.get('[formcontrolname=author]').type('Dummy Mc Dummy'))
         .then(() => cy.get('[data-testid=book_create]').click())
